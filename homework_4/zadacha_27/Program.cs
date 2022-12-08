88bf3818f2cde17b66107195b4c5e156db1bcea4
Anna <anek_ogonek@mail.ru>
2022-12-08 20:34:57 +0300

    Added zadacha 27

---
+++ homework_4/zadacha_27/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 27: 
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+//Метод поиска суммы цифр введенного числа:
+int SumDigit(int number)
+{
+    int sum = 0;
+    for (int i = 0; number > 0; i++)
+    {
+       sum = sum + number%10;
+       number = number/10; 
+    }
+    return sum;
+}
+
+//Прием числа:
+int DataEntryNumber (string str)
+{
+    Console.Write(str);
+    int number = int.Parse (Console.ReadLine());
+    return number;
+}
+
+//Программа:
+
+int number = DataEntryNumber("Введите число: ");
+int result = SumDigit(number);
+Console.Write($"Cумма цифр числа {number} равна: {result}.");
