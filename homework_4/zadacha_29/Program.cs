e2f97194bb9c5935f09049aced211af6eaaf0d64
Anna <anek_ogonek@mail.ru>
2022-12-08 20:49:35 +0300

    added zadacha_29

---
+++ homework_4/zadacha_29/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 29: 
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(col[i] + ", ");
+    }
+}
+
+//Метод, задающий массив:
+int [] NewArray(int [] array)
+{
+    int n = array.Length;
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(0,100);
+    }
+    return array;
+}
+
+//Программа:
+
+int [] array = new int [8];
+NewArray(array);
+PrintArray(array);
+Console.Write($"-> [{array[0]}, {array[1]}, {array[2]}, {array[3]}, {array[4]}, {array[5]}, {array[6]}, {array[7]}]");
