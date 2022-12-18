5bacdd0af54a9def0b7a5b2bac6204de0ac22fef
Anna <anek_ogonek@mail.ru>
2022-12-18 21:10:24 +0300

    added zadacha 41

---
+++ homework_6/zadacha_41/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача 41: 
+//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 4
+
+//Метод подсчета положительных чисел:
+int CountDigit(int [] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0) count++;
+    }
+    return count;
+}
+
+//Прием числа:
+int DataEntryNumber(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+//программа:
+
+System.Console.Write("Введите количество чисел: ");
+int size = int.Parse(Console.ReadLine());
+int [] arr =  new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = DataEntryNumber($"введите число {i}: ");
+    }
+Console.Write('[' + string.Join(", ", arr) + ']');
+int result = CountDigit(arr);
+Console.WriteLine($"-> {result} числа больше 0");
