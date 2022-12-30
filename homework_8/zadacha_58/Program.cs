c35cc35b97ee69855cb2972a82715d463b6edde3
Anna <anek_ogonek@mail.ru>
2022-12-30 18:54:07 +0300

    added zadacha 58

---
+++ homework_8/zadacha_58/Program.cs
@@ -0,0 +1,63 @@
+﻿//Задача 58: 
+//Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+//Например, даны 2 матрицы:
+//2 4 | 3 4
+//3 2 | 3 3
+//Результирующая матрица будет:
+//18 20
+//15 18
+
+//Метод, умножающий матрицу 1 на матрицу 2:
+int[,] MultMatrixs(int[,] arr1, int[,] arr2)
+{
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine(); 
+}
+
+
+
+//метод печати маcсива:
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+//метод заполнения массива:
+int[,] FillArrayWithRandomNumbers(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(leftRange, rightRange);
+        }
+    }
+    return array;
+}
+
+//программа:
+
+int leftRange = 0;
+int rightRange = 10;
+int rows1 = 4;
+int columns = 4;
+int columns2 = 4;
+
+int[,] matrix1 = FillArrayWithRandomNumbers(rows1, columns, leftRange, rightRange);
+PrintMatrix(matrix1);
+int[,] matrix2 = FillArrayWithRandomNumbers(columns, columns2, leftRange, rightRange);
+PrintMatrix(matrix2);
