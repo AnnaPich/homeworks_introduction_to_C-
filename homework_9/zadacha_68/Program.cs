b63e74f4fa1aef1babebc30c8ed06d20021d27b8
Anna <anek_ogonek@mail.ru>
2023-01-07 16:40:38 +0400

    added zadacha 68

---
+++ homework_9/zadacha_68/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 68: 
+//Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
+
+//Accerman function:
+
+int AccermanResalt(int numberM, int numberN)
+{
+    if (numberM > 0 && numberN > 0)
+    {
+        return (AccermanResalt(numberM - 1, AccermanResalt(numberM, numberN - 1)));
+    }
+    if (numberM == 0 && numberN >= 0)
+    {
+       return (numberN + 1);      
+    }
+    if (numberN == 0 && numberM > 0)
+    {
+        return(AccermanResalt(numberM - 1, 1));
+    }
+    return 0;   
+}
+
+
+//Receiving a namber from the user:
+int DataEntyNumber(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+//Program:
+int numberM = DataEntyNumber("Введите положительное натуральное число m: ");
+int numberN = DataEntyNumber("Введите положительное натуральное число n: ");
+
+int resalt = AccermanResalt(numberM, numberN);
+Console.Write(resalt);
